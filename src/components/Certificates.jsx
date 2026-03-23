fe9f14c329eb69e1283299a03f97be744fedeadd
Abhilash Jha <abhilashjha2004@gmail.com>
2026-03-24 00:01:44 +0530

    Update certificate section

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -7,9 +7,9 @@ const Certificates = () => {
     { title: "Machine Learning with Python – freeCodeCamp", url: "https://drive.google.com/file/d/1TEDT0c3ziYN9h0P6FRAi0QmFJMzG47VD/view?usp=sharing" },
     { title: "AWS Student Community Day Workshop", url: "https://drive.google.com/file/d/1bSDxUzGgAGsQiuBLWvStfOGMSIvMgB7H/view?usp=sharing" },
     { title: "Privacy and Security in Online Social Media – NPTEL", url: "https://drive.google.com/file/d/1RV1fLIaxkIM58mkC8Cg9pxsrNnFk6mix/view?usp=sharing" },
-    { title: "Computer Communications – Coursera", url: "https://drive.google.com/file/d/1rYi80Ygjb2rkmyRxvwc-E33NEIk7tiA3/view?usp=sharing" },
+    { title: "Computer Communications – Coursera", url: "https://drive.google.com/file/d/1JGHYwwSbH4LwI_yuH6bKGvJkCBBeFOuq/view?usp=sharing" },
     { title: "Responsive Web Design – freeCodeCamp", url: "https://drive.google.com/file/d/1rYi80Ygjb2rkmyRxvwc-E33NEIk7tiA3/view?usp=sharing" },
-    { title: "Google Data Analytics – Coursera", url: "https://drive.google.com/drive/folders/1NfxFJbcuBfRZn4PqqdaPNabLRpROqXsX?usp=sharing" }
+    { title: "Google Data Analytics – Coursera", url: "https://drive.google.com/file/d/1xBkS4FsqazqAjKZjAr5Fj055jn4ZSM_Y/view?usp=sharing" }
   ];
 
   return (
